a22d865aaddefdb8669e87f8aaec3e16035d0688
samBiotic <sambiotic@gmail.com>
2012-07-02 22:34:23 +0100

    updated project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject zosc "1.0.0-SNAPSHOT"
-  :description "open sound control"
+  :description "an open sound control router and scheduler for local area networks"
   :plugins [[lein-swank "1.4.4"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone/osc-clj "0.8.0"]])
